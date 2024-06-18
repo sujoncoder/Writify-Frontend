c989152a5f4bfd68e9501da8d63e4e4ae73c7989
sujonphero <sujonsheikh.in@gmail.com>
2024-06-18 20:05:53 +0600

    login page done

--- src/app/(page)/sign-up/page.js
+++ src/app/(page)/sign-up/page.js
@@ -1,43 +1,65 @@
-'use client';
-import Link from 'next/link';
-import { useState } from 'react';
+"use client";
 
-const SignUp = () => {
-  const [form, setForm] = useState({});
-  console.log(form);
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
+const Signup = () => {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  // handle change
   const handleChange = (e) => {
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
     });
   };
 
-  const handleSubmit = async (e) => {
+  // handle form submit
+  const handleOnSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
-      const response = await fetch(`http://localhost:5000/api/v1/auth/signup`, {
-        method: 'POST',
+      const res = await fetch(`http://localhost:5000/api/v1/auth/signup`, {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(form),
+        body: JSON.stringify(formData),
       });
-
-      const data = await response.json();
+      const data = await res.json();
+      setFormData({});
+      setLoading(false);
       console.log(data);
+
+      if (res.ok) {
+        toast.success("Sign-up successful!");
+        setTimeout(() => {
+          router.push("/login");
+        }, 1000);
+      } else {
+        toast.error(data.message || "Sign-up failed!");
+      }
     } catch (error) {
-      console.log(error.message);
+      toast.error("An unexpected error occurred!");
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-6 text-center">
-          SignUp to Your Account
+    <div className="bg-slate-100 px-1 sm:px-0 min-h-screen flex items-center justify-center">
+      <Toaster />
+      <div className="max-w-md w-full bg-white shadow-lg rounded-lg p-8">
+        <h2 className="text-3xl font-bold text-center text-gray-700 mb-6">
+          Sign Up
         </h2>
-        <form onSubmit={handleSubmit}>
+
+        <form onSubmit={handleOnSubmit}>
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -46,11 +68,12 @@ const SignUp = () => {
               Name
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              name="name"
-              type="name"
-              placeholder="Enter your name"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-green-500"
               onChange={handleChange}
+              required
+              id="name"
+              type="text"
+              placeholder="Enter your name"
             />
           </div>
           <div className="mb-4">
@@ -61,14 +84,15 @@ const SignUp = () => {
               Email
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              name="email"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-green-500"
+              onChange={handleChange}
+              required
+              id="email"
               type="email"
               placeholder="Enter your email"
-              onChange={handleChange}
             />
           </div>
-          <div>
+          <div className="mb-6">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
               htmlFor="password"
@@ -76,29 +100,31 @@ const SignUp = () => {
               Password
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              name="password"
-              type="password"
-              placeholder="Enter your password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:ring-2 focus:ring-green-500"
               onChange={handleChange}
+              required
+              id="password"
+              type="password"
+              placeholder="*******"
             />
+            <p className="text-gray-500 text-sm my-1">
+              Already have an account?{" "}
+              <Link
+                className="text-blue-500 hover:underline duration-300"
+                href="/login"
+              >
+                Login
+              </Link>
+            </p>
           </div>
 
-          <span className="text-slate-500">
-            ALready have an account ?{' '}
-            <Link
-              href="/login"
-              className="text-blue-500 hover:underline duration-300"
-            >
-              Login
-            </Link>
-          </span>
-          <div className="flex items-center justify-between mt-4">
+          <div className="flex items-center justify-between">
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-green-500"
               type="submit"
+              disabled={loading}
             >
-              Sign Up
+              {loading ? "loading..." : "Sign Up"}
             </button>
           </div>
         </form>
@@ -107,4 +133,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+export default Signup;
