1178f993dc610105352cae9d2e6c7bf7942a4471
kader009 <kadermollarasel000@gmail.com>
2024-06-18 18:41:03 +0600

    login page data

--- src/app/(page)/sign-up/page.js
+++ src/app/(page)/sign-up/page.js
@@ -1,13 +1,43 @@
-import Link from "next/link";
+'use client';
+import Link from 'next/link';
+import { useState } from 'react';
 
 const SignUp = () => {
+  const [form, setForm] = useState({});
+  console.log(form);
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`http://localhost:5000/api/v1/auth/signup`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(form),
+      });
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center">
           SignUp to Your Account
         </h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -17,9 +47,10 @@ const SignUp = () => {
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="name"
+              name="name"
               type="name"
               placeholder="Enter your name"
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4">
@@ -31,9 +62,10 @@ const SignUp = () => {
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="email"
+              name="email"
               type="email"
               placeholder="Enter your email"
+              onChange={handleChange}
             />
           </div>
           <div>
@@ -45,14 +77,15 @@ const SignUp = () => {
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
+              name="password"
               type="password"
               placeholder="Enter your password"
+              onChange={handleChange}
             />
           </div>
 
           <span className="text-slate-500">
-            ALready have an account ?{" "}
+            ALready have an account ?{' '}
             <Link
               href="/login"
               className="text-blue-500 hover:underline duration-300"
@@ -63,7 +96,7 @@ const SignUp = () => {
           <div className="flex items-center justify-between mt-4">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
+              type="submit"
             >
               Sign Up
             </button>
